864e42e594f21f7e0f90e9d10822bea4a8d8afe6
Vinay Gahlawat <vinay.gahlawat@gmail.com>
2025-04-28 13:23:05 -0500

    Add in placeholders for basic login and dashboard pages.

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,19 @@
+import React from "react";
+import { Routes, Route, Navigate } from "react-router-dom";
+import LoginPage from "./pages/LoginPage";
+import DashboardPage from "./pages/DashboardPage";
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/dashboard" element={<DashboardPage />} />
+        <Route path="/" element={<Navigate to="/login" />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
