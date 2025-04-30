b3265fb03588db8c716d81e046a0c6e77a102f23
Vinay Gahlawat <vinay.gahlawat@gmail.com>
2025-04-30 16:01:37 -0500

    Initial addition of a charting component (with hardcoded sample data). Using Recharts package for chart rendering as a start.

---
+++ client/src/components/StockChart.tsx
@@ -0,0 +1,39 @@
+import React from "react";
+import {
+  LineChart,
+  Line,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  Legend,
+} from "recharts";
+
+interface StockChartProps {
+  data: { name: string; price: number }[];
+}
+
+const StockChart: React.FC<StockChartProps> = ({ data }) => {
+  return (
+    <LineChart
+      width={600}
+      height={300}
+      data={data}
+      margin={{ top: 5, right: 30, left: 20, bottom: 5 }}
+    >
+      <CartesianGrid strokeDasharray="3 3" />
+      <XAxis />
+      <YAxis />
+      <Tooltip />
+      <Legend />
+      <Line
+        type="monotone"
+        dataKey="price"
+        stroke="#8884d8"
+        activeDot={{ r: 8 }}
+      />
+    </LineChart>
+  );
+};
+
+export default StockChart;
--- client/src/pages/DashboardPage.tsx
+++ client/src/pages/DashboardPage.tsx
@@ -2,6 +2,17 @@ import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { useAuth } from "../authContext";
 import TickerSelector from "../components/TickerSelector";
+import StockChart from "../components/StockChart";
+
+// TODO remove hardcoded data in favor of requested data
+const hardcodedChartData = [
+  { name: "Jan", price: 100 },
+  { name: "Feb", price: 110 },
+  { name: "Mar", price: 105 },
+  { name: "Apr", price: 120 },
+  { name: "May", price: 115 },
+  { name: "Jun", price: 130 },
+];
 
 const DashboardPage: React.FC = () => {
   const { logout } = useAuth();
@@ -25,6 +36,7 @@ const DashboardPage: React.FC = () => {
       <h1>Dashboard Page</h1>
       {selectedTicker && <p>Selected Ticker: {selectedTicker}</p>}
       <TickerSelector onTickerSelect={handleTickerSelected} />
+      <StockChart data={hardcodedChartData} />
       <button onClick={handleLogout}>Logout</button>
     </div>
   );
