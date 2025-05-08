441be8dabc2d7fa3ecdfb16b13a1b80644019835
Vinay Gahlawat <vinay.gahlawat@gmail.com>
2025-05-08 15:48:17 -0500

    (cleanup) Improve setting of server port and add TODO notes for needs prior to MVP release.

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,39 +1,12 @@
-// import express, { Request, Response } from "express";
-// import cors from "cors";
-
-// const app = express();
-// const port = process.env.PORT || 5000;
-
-// app.use(
-//   cors({
-//     origin: "*",
-//     methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-//     allowedHeaders: "Content-Type, Authorization",
-//   })
-// );
-
-// app.get("/test", (req: Request, res: Response) => {
-//   res.send("Test endpoint works!");
-// });
-
-// app.get("/api/stocks/steps/:ticker", (req: Request, res: Response) => {
-//   const ticker = req.params.ticker.toUpperCase();
-
-//   res.status(200).json({ message: `No data found for ${ticker}.` });
-// });
-
-// app.listen(port, () => {
-//   console.log(`Server listening on port ${port}...`);
-// });
 import express, { Request, Response } from "express";
 import cors from "cors";
 import hardcodedStepData from "./data/hardcodedStockStepData";
 
 const app = express();
-// const port = process.env.PORT || 5000;
-const port = 5001;
-const hostname = "127.0.0.1"; // Explicitly define the hostname
+const port: number = parseInt(process.env.port || "5001", 10);
+const hostname = "127.0.0.1"; // TODO MVP Pull appropriate hostname from env
 
+// TODO MVP Configure cors() middleware with appropriate parameters.
 app.use(cors());
 
 app.get("/test", (req: Request, res: Response) => {
